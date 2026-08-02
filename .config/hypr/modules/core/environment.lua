55b32a351c73cc8e024d16ace1bb58f78995ead9
marcello-oliva <olimars.dev@gmail.com>
2026-08-02 13:16:30 +0200

    feat(desktop/hyprland): add environment configuration settings

--- .config/hypr/modules/core/environment.lua
+++ .config/hypr/modules/core/environment.lua
@@ -5,6 +5,16 @@ hl.config({
   }
 })
 
----- ENVIRONMENT VARIABLES ------------------------------------------------------------------
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+-- ════ Compositor ════════════════════════════════════════════════
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("WLR_NO_HARDWARE_CURSORS", "1")
+
+-- ════ Nvidia ════════════════════════════════════════════════
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("GBM_BACKEND", "GBM_BACKEND")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+
+-- ════ Cursor ════════════════════════════════════════════════
+hl.env("XCURSOR_SIZE", "20")
+hl.env("HYPRCURSOR_SIZE", "20")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
