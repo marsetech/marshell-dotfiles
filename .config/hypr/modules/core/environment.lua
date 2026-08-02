f1feb3c813cc41c4242b9e020b15b3f7722b31a0
marcello-oliva <olimars.dev@gmail.com>
2026-08-02 10:58:49 +0200

    feat(desktop/hyprland): introduce ecosystem configuration

--- .config/hypr/modules/core/environment.lua
+++ .config/hypr/modules/core/environment.lua
@@ -1,3 +1,10 @@
+hl.config({
+  ecosystem = {
+    no_update_news = true,
+    no_donation_nag = true,
+  }
+})
+
 ---- ENVIRONMENT VARIABLES ------------------------------------------------------------------
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
