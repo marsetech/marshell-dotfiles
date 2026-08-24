618983c96180132afe07a04e19286836dad9791f
marcello-oliva <olimars.dev@gmail.com>
2026-08-24 16:19:59 +0200

    chore(desktop/hyprland): define custom service script paths

--- .config/hypr/shared/config/services.lua
+++ .config/hypr/shared/config/services.lua
@@ -1,5 +1,14 @@
 return {
+  status_bar_reloader = "~/.config/waybar/scripts/reload.sh",
+  system_power_menu = "~/.config/rofi/modules/system_powermenu/main.sh",
+
+  bluetooth_reloader = "~/.config/hypr/scripts/system/bluetooth-restart.service.sh",
+  audio_control_center = "~/.config/hypr/scripts/system/audio-controller.service.sh",
+
+  lockscreen_page = "pgrep -x hyprlock || hyprlock",
+
   polkit_manager = "pgrep -x polkit-gnome-au || /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &",
+
   clipboard_services = {
     "pgrep -x wl-paste || wl-paste --type text --watch cliphist store &",
     "pgrep -x wl-paste || wl-paste --type image --watch cliphist store &"
