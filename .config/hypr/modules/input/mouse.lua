fc3ab5546e4dcc9f30a454acb5d3a5c3b13829ee
marcello-oliva <olimars.dev@gmail.com>
2026-08-25 19:55:51 +0200

    fix(desktop/hyprland): quote mouse acceleration profile

--- .config/hypr/modules/input/mouse.lua
+++ .config/hypr/modules/input/mouse.lua
@@ -2,7 +2,7 @@
 hl.config({
   input = {
     follow_mouse = 1,
-    accel_profile = flat,
+    accel_profile = "flat",
     sensitivity = -0.85, -- Range: [-1,1], 0: no modification
     natural_scroll = true,
   },
