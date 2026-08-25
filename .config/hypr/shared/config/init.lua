3a0741d6c676f11faf8dde9edf96b2bd087f1084
marcello-oliva <olimars.dev@gmail.com>
2026-08-25 18:55:31 +0200

    refactor(desktop/hyprland): standardize input configuration naming

--- .config/hypr/shared/config/init.lua
+++ .config/hypr/shared/config/init.lua
@@ -3,6 +3,6 @@ local config = {}
 config.applications = require("shared.config.applications")
 config.launcher = require("shared.config.launcher")
 config.services = require("shared.config.services")
-config.workspaces = require("shared.config.workspaces")
+config.modifiers = require("shared.config.modifiers")
 
 return config
