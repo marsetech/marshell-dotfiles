7663bb24a0dbd9890b0376009cabb78cd0635aa4
marcello-oliva <olimars.dev@gmail.com>
2026-08-25 19:11:08 +0200

    feat(desktop/hyprland): use custom script as default file manager

--- .config/hypr/shared/config/applications.lua
+++ .config/hypr/shared/config/applications.lua
@@ -1,10 +1,10 @@
 local terminal_provider = "kitty"
-local file_manager_provider = "yazi"
+local file_manager_provider = "~/.config/hypr/scripts/desktop/file-manager-session-manager.sh"
 
 return {
   terminal = terminal_provider,
-  file_manager = terminal_provider .. file_manager_provider,
-  root_file_manager = terminal_provider .. " sudo -E " .. file_manager_provider,
+  file_manager = file_manager_provider .. " --user",
+  root_file_manager = file_manager_provider .. " --system",
 
   music = "spotify",
   browser = "brave-origin",
