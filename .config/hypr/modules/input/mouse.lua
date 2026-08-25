cd971f7ed12fffe20ab5d68d3a13e59594dcfa9d
marcello-oliva <olimars.dev@gmail.com>
2026-08-25 19:59:03 +0200

    chore(desktop/hyprland): adjust mouse sensitivity and scrolling

--- .config/hypr/modules/input/mouse.lua
+++ .config/hypr/modules/input/mouse.lua
@@ -3,7 +3,7 @@ hl.config({
   input = {
     follow_mouse = 1,
     accel_profile = "flat",
-    sensitivity = -0.85, -- Range: [-1,1], 0: no modification
-    natural_scroll = true,
+    sensitivity = -0.65, -- Range: [-1,1], 0: no modification
+    natural_scroll = false,
   },
 })
