49c4f3dfef1b7b8ca3810eef8f0ef2ab9f70d719
Marcello Oliva <olimars.dev@gmail.com>
2026-08-28 11:35:39 +0200

    feat(desktop/hyprland): configure personal keybindings

--- .config/hypr/modules/input/keybinds/windows.lua
+++ .config/hypr/modules/input/keybinds/windows.lua
@@ -1,9 +1,47 @@
-local mainMod = "SUPER"     -- Sets "Windows" key as main modifier
+local input = require("shared.config.modifiers")
 
-hl.bind(mainMod .. " + left", hl.dsp.focus({ direction = "left" }))
-hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
-hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
-hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
+---- Focus window movement ------------------------------------------------
+hl.bind(input.primary_key .. " + LEFT", hl.dsp.focus({ direction = "left" }))
+hl.bind(input.primary_key .. " + RIGHT", hl.dsp.focus({ direction = "right" }))
+hl.bind(input.primary_key .. " + UP", hl.dsp.focus({ direction = "up" }))
+hl.bind(input.primary_key .. " + DOWN", hl.dsp.focus({ direction = "down" }))
 
-hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
-hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
+---- Resizing windows -----------------------------------------
+local resize_value = 50
+
+hl.bind(input.secondary_key .. " + LEFT",
+  hl.dsp.window.resize({
+    x = -resize_value,
+    y = 0,
+    relative = true,
+  }),
+  { repeating = true }
+)
+hl.bind(input.secondary_key .. " + RIGHT",
+  hl.dsp.window.resize({
+    x = resize_value,
+    y = 0,
+    relative = true,
+  }),
+  { repeating = true }
+)
+hl.bind(input.secondary_key .. " + UP",
+  hl.dsp.window.resize({
+    x = 0,
+    y = -resize_value,
+    relative = true,
+  }),
+  { repeating = true }
+)
+hl.bind(input.secondary_key .. " + DOWN",
+  hl.dsp.window.resize({
+    x = 0,
+    y = resize_value,
+    relative = true,
+  }),
+  { repeating = true }
+)
+
+---- Mouse bindings actions --------------------------------------------
+hl.bind(input.primary_key .. "+ mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind(input.primary_key .. "+ mouse:273", hl.dsp.window.resize(), { mouse = true })
