d28fdebd8356fddb068a5a7573f0c4d24bbb42d5
marcello-oliva <olimars.dev@gmail.com>
2026-08-28 10:59:02 +0200

    feat(desktop/hyprland): update and extend applications keybindings

--- .config/hypr/modules/input/keybinds/applications.lua
+++ .config/hypr/modules/input/keybinds/applications.lua
@@ -1,11 +1,13 @@
----- MY PROGRAMS ----------------------------------------------
-local terminal    = "kitty"
-local fileManager = "dolphin"
-local menu        = "rofi -show drun"
+local input = require("shared.config.modifiers")
+local launcher = require("shared.config.launcher")
+local applications = require("shared.config.applications")
 
-local mainMod     = "SUPER" -- Sets "Windows" key as main modifier
+hl.bind(input.primary_key .. " + RETURN", hl.dsp.exec_cmd(applications.terminal))
+hl.bind(input.primary_key .. " + E", hl.dsp.exec_cmd(applications.file_manager))
+hl.bind(input.primary_key .. " + B", hl.dsp.exec_cmd(applications.browser))
+hl.bind(input.primary_key .. " + S", hl.dsp.exec_cmd(applications.music))
+hl.bind(input.primary_key .. " + A", hl.dsp.exec_cmd(launcher.application_selector))
+hl.bind(input.primary_key .. " + C", hl.dsp.exec_cmd(applications.editor))
+hl.bind(input.primary_key .. " + O", hl.dsp.exec_cmd(applications.screen_recording))
 
----- KEYBINDINGS ----------------------------------------------
-hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
-hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
+hl.bind(input.secondary_key .. " + E", hl.dsp.exec_cmd(applications.root_file_manager))
