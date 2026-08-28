6ff48b793e574ec03b9cfa607bb9e3e45bcfe30c
marcello-oliva <olimars.dev@gmail.com>
2026-08-28 10:59:20 +0200

    feat(desktop/hyprland): update and extend hardware keybindings

--- .config/hypr/modules/input/keybinds/hardware.lua
+++ .config/hypr/modules/input/keybinds/hardware.lua
@@ -1,14 +1,29 @@
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+local input = require("shared.config.modifiers")
+local services = require("shared.config.services")
+
+hl.bind(input.tertiary_key .. " + B", hl.dsp.exec_cmd(services.bluetooth_reloader))
+
+---- Brightness Controllers --------------------------------------------------------------
+local brightness_value = "10%"
+
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set " .. brightness_value .. "+"),
   { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set " .. brightness_value .. "-"),
+  { locked = true, repeating = true })
+
+---- Audio Volume Controllers --------------------------------------------------------------------------
+local volume_value = "5%"
+
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ " .. volume_value .. "+"),
+  { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ " .. volume_value .. "-"),
   { locked = true, repeating = true })
 hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
   { locked = true, repeating = true })
 hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
+---- Media Player Controllers ------------------------------------------------------------
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
