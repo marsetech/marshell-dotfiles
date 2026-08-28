1c457e1888fc0a2971dbeac11ac5c0ef24bde6ab
marcello-oliva <olimars.dev@gmail.com>
2026-08-28 10:58:12 +0200

    feat(desktop/hyprland): update and extend workspaces keybindings

--- .config/hypr/modules/input/keybinds/workspaces.lua
+++ .config/hypr/modules/input/keybinds/workspaces.lua
@@ -1,15 +1,12 @@
-local mainMod = "SUPER"     -- Sets "Windows" key as main modifier
+local input = require("shared.config.modifiers")
 
--- Switch workspaces with mainMod + [0-9]
--- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
   local key = i % 10 -- 10 maps to key 0
-  hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
-  hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+  hl.bind(input.primary_key .. " + " .. key, hl.dsp.focus({ workspace = i }))
+  hl.bind(input.secondary_key .. " + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
-hl.bind(mainMod .. " + S", hl.dsp.workspace.toggle_special("magic"))
-hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
+hl.bind(input.primary_key .. " + TAB", hl.dsp.focus({ workspace = "e+1" }))
 
-hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
-hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(input.primary_key .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
+hl.bind(input.primary_key .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
